4bc54c0fc5ef91b4d0548ad7934f9a0860643e26
thirupathib <thirupathibandla5@gmail.com>
2019-01-06 19:49:55 +0530

    formvlidation

---
+++ onSubmit.js
@@ -0,0 +1,31 @@
+
+    function ValidationEvent() {
+        var name = document.getElementById("name").value;
+        var email = document.getElementById("email").value;
+        var contact = document.getElementById("contact").value;
+        var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
+        
+        if (name != '' && email != '' && contact != '') {
+        if (email.match(emailReg)) {
+        if (document.getElementById("male").checked || document.getElementById("female").checked) {
+        if (contact.length == 10) {
+        alert("All type of validation has done on OnSubmit event.");
+        return true;
+        } else {
+        alert("The Contact No. must be at least 10 digit long!");
+        return false;
+        }
+        } else {
+        alert("You must select gender.....!");
+        return false;
+        }
+        } else {
+        alert("Invalid Email Address...!!!");
+        return false;
+        }
+        } else {
+        alert("All fields are required.....!");
+        return false;
+        }
+        }
+            
